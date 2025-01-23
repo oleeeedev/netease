2016927ae4507795e5bff7f9c3e2de0dd01b2423
oleeeedev <oleschumacherr1801@gmail.com>
2025-01-23 18:34:12 +0100

    Add centralized dependency management with libs.versions.toml
    Introduced a `libs.versions.toml` file for managing dependencies and version references. Updated `build.gradle.kts` to utilize the new dependency declarations, improving maintainability and consistency. Also added the Maven Publish plugin for all projects.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,28 @@ plugins {
 }
 
 group = "dev.ole.netease"
-version = "1.0-SNAPSHOT"
+version = "1.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation(platform(rootProject.libs.junitBom))
+    testImplementation(rootProject.libs.bundles.testing)
+
+    testAnnotationProcessor(rootProject.libs.lombok)
+
+    implementation(rootProject.libs.netty5)
+    implementation(rootProject.libs.bundles.utils)
+
+    annotationProcessor(rootProject.libs.lombok)
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+allprojects {
+    apply(plugin = "maven-publish")
 }
